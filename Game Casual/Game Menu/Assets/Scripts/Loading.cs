bd867ca1b79d462e43162fe3cd0b32cdf89e35eb
RamadanYosiPratama <ramadanyosi30@gmail.com>
2020-01-30 10:34:58 +0700

    Game Project

---
+++ Game Casual/Game Menu/Assets/Scripts/Loading.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+ 
+ //waktu delay untuk load scene
+ [SerializeField] private float delayLoading = 3f;
+
+ //nama scene untuk pindah sceneyang di input kan pada Inspector
+ [SerializeField] private string nameScene;
+
+ private float timeElapsed;
+
+ private void update () {
+     timeElapsed += Time.deltaTime;
+
+     if (timeElapsed > delayLoading) {
+         SceneManager.LoadScene(nameScene);
+     }
+ }    
+}
---
+++ Game Casual/Game Menu/Assets/Sprites/Scenes.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Scenes : MonoBehaviour
+{
+    //pindah scene ke menu
+    public void MenuScene() {
+        SceneManager.LoadScene("Menu");
+    }
+
+    //pindah scene ke loading
+    public void LoadingScene() {
+        SceneManager.LoadScene("Laoding");
+    }
+    //pinda ke keluar scene
+    public void KeluarScene() {
+        SceneManager.LoadScene("keluar");
+    }
+}
